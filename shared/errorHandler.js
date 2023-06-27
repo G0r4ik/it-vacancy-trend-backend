8ed7767d12524efec885ce9575dfd44c08eeab6c
Egor Gora <egorgorlushkin0@gmail.com>
2023-06-27 15:11:45 +0300

    oops забыл добавить файлики

---
+++ shared/errorHandler.js
@@ -0,0 +1,41 @@
+/* eslint-disable max-classes-per-file */
+import { sendMail } from './mail.js'
+
+export class CustomHTTPError extends Error {
+  constructor(message, httpStatusCode = 500) {
+    super(message)
+    this.name = 'CustomHTTPError'
+    this.httpStatusCode = httpStatusCode
+    this.timestamp = new Date().toISOString()
+    Error.captureStackTrace(this, this.constructor)
+  }
+
+  toString() {
+    return `${this.timestamp}\n${this.stack}`
+  }
+}
+
+export class EmptyParametersError extends CustomHTTPError {
+  constructor(message) {
+    super(message, 400)
+    this.name = 'EmptyParametersError'
+  }
+}
+
+export function errorHandler(error, req, res, next) {
+  if (!(error instanceof CustomHTTPError)) {
+    error = new CustomHTTPError(error.message)
+  }
+  console.error(error.toString())
+  const { message, httpStatusCode = 500, timestamp, stack } = error
+  sendMail({
+    subject: 'Обработана ошибка',
+    text: `${error}, ${timestamp}\n${stack}`,
+  })
+  res.status(httpStatusCode).send({
+    error: {
+      message,
+      stack: process.env.NODE_ENV === 'production' ? '' : stack,
+    },
+  })
+}
---
+++ shared/helpers.js
@@ -0,0 +1,15 @@
+import { EmptyParametersError } from './errorHandler.js'
+
+export function checkParameters(url, parameters) {
+  const emptyParameters = []
+  for (const key in parameters) {
+    if (Object.hasOwn(parameters, key) && parameters[key] === undefined) {
+      emptyParameters.push(key)
+    }
+  }
+  if (emptyParameters.length > 0) {
+    throw new EmptyParametersError(
+      `${url}. Не все параметры переданы: ${emptyParameters.join(', ')}`
+    )
+  }
+}
